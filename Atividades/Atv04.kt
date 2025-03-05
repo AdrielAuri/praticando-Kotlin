8791d276af4048a9d1c7fde750ce113ad98da083
AdrielAuri <adrielauri.dev@gmail.com>
2025-03-05 21:55:08 +0000

    atv 04 linguagem Kotlin

---
+++ Atividades/Atv04.kt
@@ -0,0 +1,12 @@
+//4 - Faça um algoritmo que mostre na tela, números inteiros de 0 a 100
+
+fun main (){
+
+    var no1 = 1
+    do
+    {
+        println("$no1")
+        no1 += 1
+    }
+    while(no1 <= 100)
+}

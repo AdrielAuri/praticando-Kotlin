374a955d232d108d7b8160a305a5203c0f063033
AdrielAuri <adrielauri.dev@gmail.com>
2025-03-17 01:04:18 +0000

    atv 08 linguagem kotlin

---
+++ Atividades/Atv08.kt
@@ -0,0 +1,12 @@
+/*8- Imprimir Números de 10 a 1, usando loop for.*/
+
+import kotlin.math.sqrt //Necessário importar a biblioteca sqrt para calcular a raiz quadrada
+
+fun main() {
+
+    for(i in 10 downTo 1){
+        
+        println(i)
+    }
+
+}
